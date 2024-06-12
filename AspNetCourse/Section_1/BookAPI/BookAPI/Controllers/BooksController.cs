12815844244adc60f2691ae1193e03a396cfe2c4
Christian Ramos <christianramoslopez10@gmail.com>
2024-06-11 22:45:38 -0400

    Initialized Asp Net Course

---
+++ AspNetCourse/Section_1/BookAPI/BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace BookAPI.Controllers
+{
+    //https://localhost:7033/api/books:
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+    }
+}
